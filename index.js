c7a5b37043df1682a17ff27c6308e9ba0d3d849c
Daniel Riccio <ricciodaniel98@gmail.com>
2023-02-27 11:45:26 -0500

    stuff  1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const commits = process.env.COMMITS;
   console.log("HELLO ${commits}");
-  core.setOutput("time", commits);
+  core.setOutput("Commits:", commits);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
