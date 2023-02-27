42de0ed0c3261a2a803ef27cd0d6c9e06a6b4deb
Daniel Riccio <ricciodaniel98@gmail.com>
2023-02-27 11:24:47 -0500

    update to use commits

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const commits = core.getInput("commits");
+  core.setOutput("commits", nameToGreet);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
